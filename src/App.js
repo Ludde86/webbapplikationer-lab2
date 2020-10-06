5aefb8715a993a16489aa587a6789aa71189f31b
Ludde86 <bslbjorn@gmail.com>
2020-10-06 10:28:50 +0200

    add display books component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
 import React, { Component } from 'react';
-import Header from './components/ui/Header/Header';
-import Form from './components/ui/Form/Form';
+import Header from './components/Header/Header';
+import Form from './components/Form/Form';
+import DisplayBooks from './components/DisplayBooks/DisplayBooks';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<Header />
-				<div className="container">
-					<div className="row form-section">
-						<Form />
-					</div>
-				</div>
-				<div className="display-books">
-					<div className="container">
-						<div className="col-12">
-							<ul className="list-group">
-								<li className="list-item list-group-item d-flex align-items-center">
-									<strong className="title">Titel</strong>
-
-									<div className="author">Författare</div>
-
-									<div className="buttons">
-										<button type="button" className="btn btn-success">
-											Editera
-										</button>
-										<button type="button" className="btn btn-danger">
-											Ta bort
-										</button>
-									</div>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
+				<Form />
+				<DisplayBooks />
 			</div>
 		);
 	}
