426cedbc9b4e7deda1c5f64c845b2b437c56600c
Ludde86 <bslbjorn@gmail.com>
2020-10-06 10:20:41 +0200

    add form component

--- src/App.js
+++ src/App.js
@@ -1,70 +1,42 @@
-import React, { Component } from 'react'
-import Header from './components/ui/Header/Header'
+import React, { Component } from 'react';
+import Header from './components/ui/Header/Header';
+import Form from './components/ui/Form/Form';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <div className="container">
-          <div className="row form-section">
-            <form className="book-form col-6">
-              <legend>Lägg till dina favoritböcker</legend>
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="title"
-                  aria-describedby="title"
-                  placeholder="Lägg till titel"
-                />
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<div className="container">
+					<div className="row form-section">
+						<Form />
+					</div>
+				</div>
+				<div className="display-books">
+					<div className="container">
+						<div className="col-12">
+							<ul className="list-group">
+								<li className="list-item list-group-item d-flex align-items-center">
+									<strong className="title">Titel</strong>
 
-                <input
-                  type="text"
-                  className="form-control"
-                  id="author"
-                  rows="3"
-                  data-gramm="true"
-                  data-txt_gramm_id="63b74fb6-c7e4-7f0e-0c1f-438d47ac87a0"
-                  data-gramm_id="63b74fb6-c7e4-7f0e-0c1f-438d47ac87a0"
-                  data-gramm_editor="true"
-                  placeholder="Lägg till författare"
-                />
-              </div>
-              <button
-                type="submit"
-                className="btn btn-primary btn-lg btn-block"
-              >
-                Skicka
-              </button>
-            </form>
-          </div>
-        </div>
-        <div className="display-books">
-          <div className="container">
-            <div className="col-12">
-              <ul className="list-group">
-                <li className="list-item list-group-item d-flex align-items-center">
-                  <strong className="title">Titel</strong>
+									<div className="author">Författare</div>
 
-                  <div className="author">Författare</div>
-
-                  <div className="buttons">
-                    <button type="button" className="btn btn-success">
-                      Editera
-                    </button>
-                    <button type="button" className="btn btn-danger">
-                      Ta bort
-                    </button>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
+									<div className="buttons">
+										<button type="button" className="btn btn-success">
+											Editera
+										</button>
+										<button type="button" className="btn btn-danger">
+											Ta bort
+										</button>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
