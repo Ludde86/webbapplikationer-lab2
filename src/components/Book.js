61a0d9a14470a87bdc3b65e6a662f0e562e3fa5e
Ludde86 <bslbjorn@gmail.com>
2020-10-07 14:07:33 +0200

    fix pass old value into edit input

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,35 +1,28 @@
 import React, { useState } from 'react';
 
 const Book = ({ book, handleRemoveBook, handleUpdateBook, setIsEdit, isEdit }) => {
-	const [ newTitle, setNewTitle ] = useState('');
-	const [ newAuthor, setNewAuthor ] = useState('');
-	console.log(isEdit);
+	const [ newTitle, setNewTitle ] = useState(book.title);
+	const [ newAuthor, setNewAuthor ] = useState(book.author);
 	return (
 		<li className="list-item list-group-item d-flex align-items-center">
 			{isEdit.open ? (
-				<div className="form-group">
+				<div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '70%' }}>
 					<input
 						type="text"
 						className="form-control"
-						aria-describedby="title"
-						placeholder="Ändra titel"
+						value={newTitle}
 						onChange={(e) => setNewTitle(e.target.value)}
 					/>
 
 					<input
 						type="text"
 						className="form-control"
-						rows="3"
-						data-gramm="true"
-						data-txt_gramm_id="63b74fb6-c7e4-7f0e-0c1f-438d47ac87a0"
-						data-gramm_id="63b74fb6-c7e4-7f0e-0c1f-438d47ac87a0"
-						data-gramm_editor="true"
-						placeholder="Ändra författare"
+						value={newAuthor}
 						onChange={(e) => setNewAuthor(e.target.value)}
 					/>
 				</div>
 			) : (
-				<div>
+				<div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '80%' }}>
 					<strong className="title"> {book.title} </strong>
 
 					<div className="author"> {book.author} </div>
@@ -38,26 +31,36 @@ const Book = ({ book, handleRemoveBook, handleUpdateBook, setIsEdit, isEdit }) =
 
 			<div className="buttons">
 				{isEdit.open ? (
-					<button
-						type="button"
-						className="btn btn-success"
-						onClick={() => handleUpdateBook(book.id, newTitle, newAuthor)}
-					>
-						Uppdatera
-					</button>
+					<div>
+						<button
+							type="button"
+							className="btn btn-success"
+							onClick={() => handleUpdateBook(book.id, newTitle, newAuthor)}
+						>
+							Uppdatera
+						</button>
+						<button
+							type="button"
+							className="btn btn-danger"
+							onClick={() => setIsEdit({ open: false, selectedId: null })}
+						>
+							Stäng
+						</button>
+					</div>
 				) : (
-					<button
-						type="button"
-						className="btn btn-success"
-						onClick={() => setIsEdit({ open: true, selectedId: book.id })}
-					>
-						Editera
-					</button>
+					<div>
+						<button
+							type="button"
+							className="btn btn-success"
+							onClick={() => setIsEdit({ open: true, selectedId: book.id })}
+						>
+							Editera
+						</button>
+						<button type="button" className="btn btn-danger" onClick={() => handleRemoveBook(book.id)}>
+							Ta Bort
+						</button>
+					</div>
 				)}
-
-				<button type="button" className="btn btn-danger" onClick={() => handleRemoveBook(book.id)}>
-					Ta bort
-				</button>
 			</div>
 		</li>
 	);
